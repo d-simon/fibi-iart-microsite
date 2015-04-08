9cd110580d0509a136cc04f6c6739027aa8fd8eb
David Simon <me@davidsimon.ch>
2015-04-08 09:51:38 +0200

    [Update] Cursors: Add more cursors to demo

--- src/js/app/main/index.js
+++ src/js/app/main/index.js
@@ -2,13 +2,36 @@ import $ from 'jquery';
 
 
 $(document).ready(function () {
-    var $body = $(document.body);
+    var $body = $(document.body),
+        $nono = $('.nono');
 
-    $body.mousedown(function(){
+    $body.on('mousedown', function () {
         $body.toggleClass('is-mouse-down', true);
-    }).mouseup(function(){
-        $body.toggleClass('is-mouse-down', false);
-    }).mouseout(function(){
+    }).on('mouseup mouseout', function () {
         $body.toggleClass('is-mouse-down', false);
     });
+
+    var stepper = (function () {
+            var state = false;
+            return function () {
+                state = !state;
+                return state;
+            };
+        })(),
+        interval;
+
+    $nono.on('mouseenter', function () {
+        interval = setInterval(function () {
+                var state = stepper();
+                $body
+                    .toggleClass('is-nono-hover', !state)
+                    .toggleClass('is-nono-hover-2', state);
+            }, 400);
+    }).add($body).on('mouseout', function () {
+        clearInterval(interval);
+        $body
+            .toggleClass('is-nono-hover-2', false)
+            .toggleClass('is-nono-hover', false);
+    });
+
 });
